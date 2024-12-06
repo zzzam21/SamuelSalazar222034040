bac9aadff95c285a5b0f3e7c0b9f50b4a1ddf5ef
zzzam21 <jonas01311@gmail.com>
2024-12-05 23:35:14 -0500

    feat: scriptBase y API

---
+++ biblioteca/API_Library/API_Library/Controllers/booksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Library.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class booksController : ControllerBase
+  {
+  }
+}
---
+++ biblioteca/API_Library/API_Library/books.cs
@@ -0,0 +1,12 @@
+namespace API_Library
+{
+  public class books
+  {
+    public string Id { get; set; }
+    public string tittle { get; set; }
+    public string author { get; set; }
+    public string editorial { get; set; }
+    public int pages { get; set; }
+
+  }
+}
